a86baf8dd12f027c71fbc4fddbf59e1804b3b9b8
Aiman Madan <64807286+AimanMadan@users.noreply.github.com>
2024-06-18 02:52:23 -0700

    Update main.py
    Publish function needs work, publish button xpath needs update

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ import time
 import os
 import glob
 
-
 # Setup Chrome WebDriver
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
-
-
 
 # Function to get email and password from a text file
 def get_credentials(filename):
@@ -26,7 +23,7 @@ def get_credentials(filename):
         password = lines[1].strip()
     return email, password
 
-#Function to log in
+# Function to log into Beatstars
 def beatstars_sign_in(driver, email, password):
     # Open BeatStars website
     driver.get("https://www.beatstars.com/dashboard")
@@ -72,7 +69,7 @@ def beatstars_sign_in(driver, email, password):
     # Click the "Continue" button
     continue_button.click()
     
-# Function to get .wav, stem files (.zip or .rar), and artwork files
+# Function to get .wav, stem files (.zip or .rar), and artwork (.jpeg or .jpg or.png) files
 def get_files(directory):
     wav = glob.glob(os.path.join(directory, "*.wav"))
     stems = glob.glob(os.path.join(directory, "*.zip")) + glob.glob(os.path.join(directory, "*.rar"))
@@ -83,9 +80,9 @@ def get_files(directory):
     
     return wav, stems, artwork
     
-#Upload a track with the attached files (Must have .wav at least)
-def upload_track(driver, wav):
-    wait = WebDriverWait(driver, 10)
+#Upload a track with the attached files(Must have .wav at least)
+def upload_track(driver, wav, stems):
+    wait = WebDriverWait(driver, 50)
     
     # Wait for the "Create +" button to be clickable using the provided XPath
     create_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='app-body']/mp-root/mp-main-menu-top-nav/header/div/bs-container-grid[1]/div/div[3]/div/bs-menu-more-options/div/bs-square-button/button")))
@@ -99,34 +96,68 @@ def upload_track(driver, wav):
     # Click the "Create Track" button
     create_track_button.click()
         
-    # Wait for the "Upload" button to be clickable using the provided XPath
-    upload_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='cdk-drop-list-0']/studio-form-file-box/div/div/div[2]/bs-upload-button/bs-universal-upload-button/div/div/bs-square-button/button")))
-    print("Upload button located, attempting to click...")
+    # Wait for the "Upload wav" button to be clickable using the provided XPath
+    upload_wav_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='cdk-drop-list-0']/studio-form-file-box/div/div/div[2]/bs-upload-button/bs-universal-upload-button/div/div/bs-square-button/button")))
+    print("Upload wav button located, attempting to click...")
     # Click the "Upload" button
-    upload_button.click()
+    upload_wav_button.click()
     
     # Wait for the "Drag & Drop" button to be clickable using the provided XPath
-    drag_and_drop_input = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='uppy-drag-drop']/div/input")))
+    drag_and_drop_input = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='uppy-drag-drop']/div/button/input")))
     print("Drag & Drop input located, attempting to upload file...")
+    
+    
     # Upload the .wav file
     drag_and_drop_input.send_keys(wav)
     print(f"Uploaded wav file")
+    
+    # Check if stems are present and upload them
+    if stems:
+        # Wait for the "Upload stems" button to be clickable using the provided XPath
+        upload_stems_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='cdk-drop-list-40']/studio-form-file-box/div/div/div[2]/bs-upload-button/bs-universal-upload-button/div/div/bs-square-button/button")))
+        print("Upload stems button located, attempting to click...")
+        # Click the "Upload" button
+        upload_stems_button.click()
+        
+        # Wait for the "Drag & Drop" input to be present using the provided XPath
+        drag_and_drop_input_stems = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='uppy-drag-drop']/div/button/input")))
+        print("Drag & Drop input for stems located, attempting to upload file...")
+
+        # Upload the stem file
+        drag_and_drop_input_stems.send_keys(stems) 
+        print(f"Uploaded stems file")
+    else:
+        print("No stems found. Skipping stem upload.")
 
     # Sleep to observe the final state
-    time.sleep(10)  # Sleep for 10 seconds
+    time.sleep(50)  # Sleep for 10 seconds
 
+# Function to publish track
+"""def publish_track(driver):
+    
+    # Wait for the "Publish" button to be clickable using the provided XPath
+    publish_button_input = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='uppy-drag-drop']/div/button/input")))
+    print("Publish input located, attempting to Publish file...")
+    
+    
+    # Sleep to observe the final state
+    time.sleep(50)  # Sleep for 10 seconds
+    """
 try:
     # Get email and password from Login.txt
     email, password = get_credentials('Login.txt')
+    
     # Get .wav, stem, and artwork files from the specified directory
     wav, stems, artwork = get_files('C:/Users/aiman/Documents/BeatStarsSingleClick/Files To Upload') 
     
-    
-    
     # Call the login function
     beatstars_sign_in(driver, email, password)
-    # Call the function to upload the tracks
-    upload_track(driver, wav)
+    
+    # Call the function to upload the track
+    upload_track(driver, wav, stems)
+    
+    # Call the function to publish the track
+    #publish_track(driver)
     
 finally:
     # Quit the driver
